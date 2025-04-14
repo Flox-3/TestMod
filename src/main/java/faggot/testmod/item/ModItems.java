bbcbdf786c10ad526da80ef7c551c8956c8dead0
Flox-3 <fee2002lix@gmail.com>
2025-04-14 11:34:29 +0200

    Fortnite

--- src/main/java/faggot/testmod/item/ModItems.java
+++ src/main/java/faggot/testmod/item/ModItems.java
@@ -10,7 +10,7 @@ import net.minecraft.util.Identifier;
 
 public class ModItems {
     public static final Item RAW_OSMIUM = registerItem("raw_osmium", new Item(new Item.Settings()));
-
+    public static final Item OSMIUM_INGOT = registerItem("osmium_ingot", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TestMod.MOD_ID, name), item);
@@ -21,6 +21,7 @@ public class ModItems {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
             fabricItemGroupEntries.add(RAW_OSMIUM);
+            fabricItemGroupEntries.add(OSMIUM_INGOT);
         });
     }
 }
