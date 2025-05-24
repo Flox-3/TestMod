aadafae6107ef0bd4aa69f50b1ab86f9d8809664
Flox-3 <fee2002lix@gmail.com>
2025-05-24 15:06:58 +0200

    Kuchen

--- src/main/java/faggot/testmod/block/custom/GrowthChamberCasing.java
+++ src/main/java/faggot/testmod/block/custom/GrowthChamberCasing.java
@@ -1,33 +1,44 @@
 package faggot.testmod.block.custom;
 
+import com.mojang.serialization.MapCodec;
 import faggot.testmod.block.entity.custom.GrowthChamberCasingEntity;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.BlockWithEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-public class GrowthChamberCasing extends Block {
+public class GrowthChamberCasing extends BlockWithEntity {
     public GrowthChamberCasing(Settings settings) {
         super(settings);
     }
 
     @Override
-    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean notify) {
-        super.neighborUpdate(state, world, pos, block, fromPos, notify);
-
-        if (!world.isClient) {
-            BlockEntity blockEntity = world.getBlockEntity(pos);
-            if (blockEntity instanceof GrowthChamberCasingEntity casing) {
-                casing.checkForCoreBlocks();
-            }
-        }
+    protected MapCodec<? extends BlockWithEntity> getCodec() {
+        return null;
     }
 
+    public ActionResult onUse(BlockState state, World world, BlockPos pos,
+                              PlayerEntity player, Hand hand, BlockHitResult hit) {
+        if (!world.isClient) {
+            player.sendMessage(Text.literal("This is a casing block."), false);
+        }
+        return ActionResult.SUCCESS;
+    }
+
+
+
     @Override
-    public void onPlaced(World world, BlockPos pos, BlockState state, LivingEntity placer, ItemStack itemStack) {
+    public void onPlaced(World world, BlockPos pos, BlockState state,
+                         LivingEntity placer, ItemStack itemStack) {
         super.onPlaced(world, pos, state, placer, itemStack);
 
         if (!world.isClient) {
@@ -39,4 +50,8 @@ public class GrowthChamberCasing extends Block {
     }
 
 
+    @Override
+    public @Nullable BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
+        return null;
+    }
 }
